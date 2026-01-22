4b975ceed6ea803fd7d0b2333bc12f6cef813328
tt-nxt <64884270+TatuPekkarinen@users.noreply.github.com>
2026-01-22 14:21:47 +0200

    Refactor CORDIC

--- main.py
+++ main.py
@@ -6,25 +6,25 @@ class vector_v0():
         self.x = 1.0
         self.y = 0.0
 
-def atan_calculation(i):
-    return [atan(2**-i) for i in range(i)]
+def atan_calculation(iters):
+    return [atan(2**-i) for i in range(iters)]
 
-def CORDIC_gain(i):
+def CORDIC_gain(iters):
     val = 1.0
-    for i in range(i):
+    for i in range(iters):
         exp_2i = -2 * i
         val *= 1 / sqrt(1 + 2 ** (exp_2i))
     return val
 
 def CORDIC(theta):
-    i = 50
+    iters = 50
     v = vector_v0()
-    K = CORDIC_gain(i)
+    K = CORDIC_gain(iters)
     x = v.x * K
     y = v.y * K
 
-    atan_calc = atan_calculation(i)
-    for i in range(i):
+    atan_calc = atan_calculation(iters)
+    for i in range(iters):
         shift = 1 if theta >= 0 else -1
         xn = x - shift * y * (2 ** -i)
         yn = y + shift * x * (2 ** -i)
@@ -32,19 +32,16 @@ def CORDIC(theta):
         x, y, theta = xn, yn, theta_n
     return x, y
 
+def exit():
+    sys.exit(0)
+    return
+
 def main(RED, GREEN, RESET):
     try:
-        try:
-            theta_input = int(input("pi / "))
-            print()
-            theta = pi / theta_input
-
-        except ValueError:
-            print(f"{RED}\nInvalid Value\n{RESET}")
-            return
-        else: 
-            print(f"{RED}Invalid input{RESET}")
-            
+        input_theta = input("pi / ") 
+        if input_theta == 'exit': exit()
+        if input_theta == 0 : raise
+        theta = pi / int(input_theta)
         cosine_theta, sine_theta = CORDIC(theta)
 
         print(f"{GREEN}CORDIC Algorithm Values{RESET}")
@@ -55,9 +52,9 @@ def main(RED, GREEN, RESET):
         print(f"Math Library -> cosine : {cos(theta)}")
         print(f"Math Library -> sine : {sin(theta)}\n")
 
-    except KeyboardInterrupt:
-        print(f"{RED}KeyboardInterrupt{RESET}")
-        sys.exit(0)
+    except ValueError:
+        print(f"{RED}\nInvalid Value\n{RESET}")
+        return
 
 
 if __name__ == "__main__":
@@ -66,4 +63,7 @@ if __name__ == "__main__":
     RESET = '\033[0m'
     print(f"{GREEN}CORDIC Algorithm{RESET}")
     while True:
-        main(RED, GREEN, RESET)
+        try: main(RED, GREEN, RESET)
+        except KeyboardInterrupt: 
+            print(f"\n{RED}KeyboardInterrupt{RESET}")
+            exit()
