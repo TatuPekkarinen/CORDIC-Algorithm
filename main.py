21af30dda2eacf9eef590678bcd2659a02c6631a
tt-nxt <64884270+TatuPekkarinen@users.noreply.github.com>
2026-01-22 13:11:10 +0200

    Refactor CORDIC

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-from math import*
+from math import sqrt, atan, pi, cos, sin
 import sys
-
 
 class vector_v0():
     def __init__(self):
         self.x = 1.0
         self.y = 0.0
 
-def atan_calculation(iter):
-    return [atan(2**-i) for i in range(iter)]
+def atan_calculation(i):
+    return [atan(2**-i) for i in range(i)]
 
-def CORDIC_gain(iter):
+def CORDIC_gain(i):
     val = 1.0
-    for i in range(iter):
+    for i in range(i):
         exp_2i = -2 * i
         val *= 1 / sqrt(1 + 2 ** (exp_2i))
     return val
 
 def CORDIC(theta):
-    iter = 50
+    i = 50
     v = vector_v0()
-    K = CORDIC_gain(iter)
+    K = CORDIC_gain(i)
     x = v.x * K
     y = v.y * K
 
-    atan_calc = atan_calculation(iter)
-    for iteration in range(iter):
+    atan_calc = atan_calculation(i)
+    for iation in range(i):
         shift = 1 if theta >= 0 else -1
-        xn = x - shift * y * (2 ** -iteration)
-        yn = y + shift * x * (2 ** -iteration)
-        theta_n = theta - shift * atan_calc[iteration]
+        xn = x - shift * y * (2 ** -iation)
+        yn = y + shift * x * (2 ** -iation)
+        theta_n = theta - shift * atan_calc[iation]
         x, y, theta = xn, yn, theta_n
     return x, y
 
@@ -38,14 +37,14 @@ def main(RED, GREEN, RESET):
         try:
             theta_input = int(input("pi / "))
             print()
+            theta = pi / theta_input
 
         except ValueError:
             print(f"{RED}\nInvalid Value\n{RESET}")
             return
-
-        if theta_input == int(theta_input):
-            theta = pi / theta_input
-        else: print(f"{RED}Invalid input{RESET}")
+        else: 
+            print(f"{RED}Invalid input{RESET}")
+            
         cosine_theta, sine_theta = CORDIC(theta)
 
         print(f"{GREEN}CORDIC Algorithm Values{RESET}")
