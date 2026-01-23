447dfdf3afb86c27a3eab4ba23aad0debfc2675a
TP44 <tatupek7@gmail.com>
2026-01-23 10:52:32 +0200

    Lates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### CORDIC Algorithm
 > CORDIC algorithm that calculates sin & cos. Read couple of math papers on this and tried to do my best on this implementation. Works by magic and carried heavily by mathematicians who came up with this so im not the wizard.
+
+Only works with 0 - 89+ / 0 - 89- (degrees)
--- main.py
+++ main.py
@@ -32,17 +32,15 @@ def CORDIC(theta):
         x, y, theta = xn, yn, tn
     return x, y
 
-def exit():
+def quit_program():
     sys.exit(0)
     return
 
 def main(RED, GREEN, RESET):
     try:
-        theta_i = int(input("INPUT pi / "))
-        if theta_i == 0:
-            print(f"{RED}\nInvalid Value\n{RESET}")
-            exit()
-        theta = pi / int(theta_i)
+        theta_degrees = float(input("Enter angle in degrees: "))
+        theta = theta_degrees * pi / 180
+        theta = ((theta + pi) % (2 * pi)) - pi
         cosine_theta, sine_theta = CORDIC(theta)
 
         print(f"{GREEN}CORDIC Algorithm Values{RESET}")
@@ -62,9 +60,10 @@ if __name__ == "__main__":
     RED = "\033[0;31m"
     GREEN = "\033[0;32m"
     RESET = '\033[0m'
+
     print(f"{GREEN}CORDIC Algorithm{RESET}")
     while True:
         try: main(RED, GREEN, RESET)
         except KeyboardInterrupt: 
             print(f"\n{RED}KeyboardInterrupt{RESET}")
-            exit()
+            quit_program()
