03698814bda6ad617c42f614db90ec85bf95461d
TTP04 <tatupek7@gmail.com>
2026-03-10 16:44:00 +0200

    Better implementations

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### CORDIC Algorithm
-> CORDIC algorithm that calculates sin & cos. Read couple of math papers on this and tried to do my best on this implementation. Works by magic and carried heavily by mathematicians who came up with this so im not the wizard.
+> CORDIC algorithm that calculates sin & cos. Read couple of math papers on this and tried to do my best on this implementation. Works by magic and carried heavily by mathematicians who came up with this.
 
-*Only works with 0 - 89+ / 0 - 89- (degrees) and some day will fix it (maybe)*
+Currently limited to ±99° due to CORDIC limitations. Will fix it someday.
--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 from math import sqrt, atan, pi, cos, sin, radians
 import sys
 
+#the margin of error
 epsilon = 1e-9
+
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+RESET = '\033[0m'
 
 class v_v0():
     def __init__(self):
         self.x = 1.0
         self.y = 0.0
 
-def ATAN_CALC(iters):
-    return [atan(2**-i) for i in range(iters)]
+def ATAN_CALC(itr):
+    return [atan(2**-i) for i in range(itr)]
 
-def C_GAIN(iters):
+def C_GAIN(itr):
     val = 1.0
-    for i in range(iters):
+    for i in range(itr):
         exp_2i = -2 * i
         val *= 1 / sqrt(1 + 2 ** (exp_2i))
     return val
 
 def CORDIC(theta):
-    iters = 40
+    itr = 40
     v = v_v0()
-    K = C_GAIN(iters)
+    K = C_GAIN(itr)
     x = v.x * K
     y = v.y * K
 
-    atan_calc = ATAN_CALC(iters)
-    for i in range(iters):
+    atan_calc = ATAN_CALC(itr)
+    for i in range(itr):
         shift = 1 if theta >= 0 else -1
         xn = x - shift * y * (2 ** -i)
         yn = y + shift * x * (2 ** -i)
@@ -35,45 +40,41 @@ def CORDIC(theta):
     return x, y
 
 def absolute_value(n):
-    if (n * -1) >= 0:
-         return n * -1
-    else: 
-        return n
+    if (n * -1) >= 0: return n * -1
+    else: return n
 
+#limit values so smaller than epsilon is just zero
 def epsilon_clamp(n):
-    if absolute_value(n) < epsilon:
+    if absolute_value(n) <= epsilon:
         return 0.0
     return n
 
-def main(RED, GREEN, RESET):
+def main():
     try:
-        theta_degrees = float(input("Enter Angle In Degrees: "))
-        theta = radians(theta_degrees)
+        degree = float(input("Enter Angle In Degrees: "))
+        theta = degree * (pi / 180)
         cordic_cos, cordic_sin = CORDIC(theta)
 
         cordic_cos = epsilon_clamp(cordic_cos)
         cordic_sin = epsilon_clamp(cordic_sin)
+
         print(f"{GREEN}CORDIC Algorithm Values{RESET}")
         print(f"CORDIC -> Cosine : {cordic_cos}")
         print(f"CORDIC -> Sine : {cordic_sin}\n")
 
         print(f"{GREEN}Math Library Function{RESET}")
-        print(f"Math Library -> Cosine : {cos(theta)}")
-        print(f"Math Library -> Sine : {sin(theta)}\n")
+        print(f"Math Library -> Cosine : {epsilon_clamp(cos(theta))}")
+        print(f"Math Library -> Sine : {epsilon_clamp(sin(theta))}\n")
 
     except ValueError:
         print(f"{RED}\nInvalid Value\n{RESET}")
         return
 
 if __name__ == "__main__":
-    RED = "\033[0;31m"
-    GREEN = "\033[0;32m"
-    RESET = '\033[0m'
-
     print(f"{GREEN}CORDIC Algorithm{RESET}")
     while True:
         try: 
-            main(RED, GREEN, RESET)
+            main()
         except KeyboardInterrupt: 
             print(f"\n{RED}KeyboardInterrupt{RESET}")
             sys.exit(0)
